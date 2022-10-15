187443df371bbfbca01aa4b8bc92ba0bcd7bfb31
Wendirad Demelash <wendiradame@gmail.com>
2022-10-15 17:56:47 +0300

    Solve `sorting-the-sentence` [LeetCode]

---
+++ week-one/sorting-the-sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        ordered = s.split()
+        for i in s.split():
+            ordered[int(i[-1]) - 1] = i[:-1]
+        return ' '.join(ordered)
