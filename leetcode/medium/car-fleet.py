e8b526b050b174f93fa91380ab69d32c5bfa2598
Wendirad Demelash <wendiradame@gmail.com>
2022-10-19 11:10:45 +0300

    Solve `car-fleet` [LeetCode]

---
+++ leetcode/medium/car-fleet.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calc(self, target, pos, speed):
+        return (target - pos) / speed
+    
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        d =  dict(zip(position, speed))
+        
+        position.sort(reverse=True)
+        
+        current = None 
+        ans = 0
+        print(position)
+        for p in position:
+            val = self.calc(target, p, d[p])
+            if current is None:
+                ans = 1
+                current = val
+            elif val > current:
+                ans += 1
+                current = val               
+            
+        return ans
