c2b17227995828737ecba8a3b35142dd1ed3b241
Wendirad Demelash <wendiradame@gmail.com>
2023-10-06 09:30:27 -0700

    Solve `python-lists` [HackerRank]

---
+++ hackerrank/easy/python-lists.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    N = int(input())
+    
+    l = []
+    for i in range(N):
+        command, *args =  input().split()
+        args =  list(map(int, args))
+        if command == 'insert':
+            ix, val = args
+            l.insert(ix, val)
+        elif command == 'print':
+            print(l)
+        elif command == 'remove':
+            l.remove(args[0])
+        elif command == 'append':
+            l.append(args[0])
+        elif command == 'sort':
+            l.sort()
+        elif command == 'pop':
+            l.pop()
+        elif command == 'reverse':
+            l.reverse()
