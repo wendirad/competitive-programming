015895a36e61e77bcebd35b0a2bc77f396c53980
Wendirad Demelash <wendiradame@gmail.com>
2023-10-08 01:12:31 -0700

    Solve `remove-smallest` [CodeForces]

---
+++ codeforces/easy/remove-smallest.py
@@ -0,0 +1,22 @@
+t = int(input())
+
+for test in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    if n == 1:
+        print("YES")
+        continue
+
+    nums.sort()
+    
+    i = 1
+    f = True
+    while i < n:
+        if abs(nums[i-1] - nums[i]) > 1:
+            print("NO")
+            f = False
+            break
+        i += 1
+    if f:
+        print("YES")
