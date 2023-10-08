98d3fc81a098d262a3876b7eddb13a3e5cb6a5ca
Wendirad Demelash <wendiradame@gmail.com>
2023-10-08 01:45:19 -0700

    Optimize `less_or_equal` [CodeForces]

--- codeforces/medium/less_or_equal.py
+++ codeforces/medium/less_or_equal.py
@@ -1,10 +1,10 @@
-from collections import defaultdict, Counter
 n, k = map(int, input().split())
 
 nums = list(map(int, input().split()))
 
 
 nums.sort()
+
 if k < 1:
     if nums[0] > 1:
         print(1)
@@ -13,11 +13,8 @@ if k < 1:
 elif k == n:
     print(nums[-1])
 else:
-    bc = Counter(nums[:k])
-    ac = Counter(nums[k:])
     el = nums[k-1]
-
-    if ac[el] != 0:
+    if nums[k] == el:
         print(-1)
     else:
         print(el)
