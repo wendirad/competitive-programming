6b8f2b3cb6397606d4fac6a28415b69e8ec5cb33
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `final-value-of-variable-after-performing-operations` [LeetCode]

---
+++ leetcode/easy/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(string op: operations){
+            if (op[1] == '+')
+                x++;
+            else
+                x--;
+        }
+        return x;
+    }
+};
