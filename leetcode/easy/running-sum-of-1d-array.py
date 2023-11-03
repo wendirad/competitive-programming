ed6db612dfd06433903ee667d231c115ae1f3679
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `running-sum-of-1d-array` [LeetCode]

---
+++ leetcode/easy/running-sum-of-1d-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        ans = [nums[0]]
+        for i in nums[1:]:
+            ans.append(ans[-1] + i)
+        
+        return ans
