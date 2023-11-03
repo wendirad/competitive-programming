9dcdc5e0cb3531a975024bd394ef24d85ac25d88
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `shuffle-string` [LeetCode]

---
+++ leetcode/easy/shuffle-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        n = [0] * len(s)
+        for i, idx in enumerate(indices):
+            n[idx] = s[i] 
+        
+        return ''.join(n)
