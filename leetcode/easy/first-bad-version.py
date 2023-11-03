867aa074afe4323ef24a618f7878f1a84546b7fe
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `first-bad-version` [LeetCode]

---
+++ leetcode/easy/first-bad-version.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        self.left = 1
+        self.right = n
+        self.bad_version = float('inf')
+        return self.binary_search()
+    
+    def binary_search(self):
+        if self.right < self.left:
+            return self.bad_version
+        
+        mid = (self.left + self.right) // 2
+        
+        if isBadVersion(mid):
+            self.right = mid - 1
+            self.bad_version = min(self.bad_version, mid)
+        else:
+            self.left = mid + 1
+        return self.binary_search()
