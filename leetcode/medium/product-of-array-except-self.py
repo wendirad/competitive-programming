37f806c69a55e88f8ddbe51239a1f56ae9e3bcfa
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:06 -0700

    Solve `product-of-array-except-self` [LeetCode]

---
+++ leetcode/medium/product-of-array-except-self.py
@@ -0,0 +1,13 @@
+from functools import reduce
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pre = [1] * n
+        post = [1] * n
+
+
+        for i in range(1, n):
+            pre[i] = pre[i-1] * nums[i-1]
+            post[n-i-1] = post[n-i] * nums[n - i]
+        
+        return [pre[i] * post[i] for i in range(n)]
