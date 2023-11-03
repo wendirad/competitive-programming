2e6b203b191b85240daeebd366d7f259b788fbef
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `contains-duplicate` [LeetCode]

---
+++ leetcode/easy/contains-duplicate.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dup = defaultdict(int)
+
+        for num in nums:
+            dup[num] += 1
+            if dup[num] > 1:
+                return True
+        
+        return False
