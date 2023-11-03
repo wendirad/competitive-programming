da868c1c8b4110e476dfc27f2dd1d33e57afbf08
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `maximum-number-of-words-found-in-sentences` [LeetCode]

---
+++ leetcode/easy/maximum-number-of-words-found-in-sentences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        MAX = float('-inf')
+
+        for st in sentences:
+            MAX = max(MAX, len(st.split()))
+        
+        return MAX
