815b607b952ff53a777aa2df89e999555175e17e
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `sum-of-all-odd-length-subarrays` [LeetCode]

---
+++ leetcode/easy/sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+
+        res = 0
+
+        for i in range(n):
+            start = n - i
+            end = i + 1
+            total = start * end
+
+            if total % 2 == 0:
+                total /= 2
+            else:
+                total = total // 2 + 1
+            
+            res += arr[i] * total
+        
+        return int(res)
