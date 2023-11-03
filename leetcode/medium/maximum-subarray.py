bf1ae20e15a7bc992e94c95fa04f5d26438908e7
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:06 -0700

    Solve `maximum-subarray` [LeetCode]

---
+++ leetcode/medium/maximum-subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        best_sum = float('-inf')
+        current_sum = 0
+        for i in nums:
+            current_sum = max(i, current_sum + i)
+            best_sum = max(best_sum, current_sum)
+        return best_sum
