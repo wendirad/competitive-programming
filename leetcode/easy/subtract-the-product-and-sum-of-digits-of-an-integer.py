9586e1277ef73dea3a4ecd6760190faad2930cc1
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `subtract-the-product-and-sum-of-digits-of-an-integer` [LeetCode]

---
+++ leetcode/easy/subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        S = 0
+        P = 1
+        
+        while n != 0:
+            last = n % 10
+            n //= 10
+            S += last
+            P *= last        
+        return P - S
