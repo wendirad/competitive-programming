74091cac3f0151e1bd90106ae07809108fa88c32
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `unique-morse-code-words` [LeetCode]

---
+++ leetcode/easy/unique-morse-code-words.py
@@ -0,0 +1,11 @@
+import string
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        mores = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+        uniques = set()
+
+        for word in words:
+            uniques.add(''.join(map(lambda ch: mores[ord(ch) - 97], word)))
+        
+        return len(uniques)
