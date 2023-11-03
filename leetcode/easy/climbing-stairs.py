39165dd55c65316d45a52ebae60dd26625d77f7f
Wendirad Demelash <wendiradame@gmail.com>
2023-11-03 00:01:05 -0700

    Solve `climbing-stairs` [LeetCode]

---
+++ leetcode/easy/climbing-stairs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        fibs = [1, 1]
+        for i in range(2, n+1):
+            fibs.append(fibs[-1] + fibs[-2])
+        return fibs[n]
